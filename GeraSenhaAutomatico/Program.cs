53ba941063ac54090d172af98c4b35fdd915ee21
Richard <Richard@DESKTOP-9LM3OJE>
2021-02-01 01:03:01 -0300

    Adicionar arquivos de projeto.

---
+++ GeraSenhaAutomatico/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GeraSenhaAutomatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o tamanho da senha: \n");
+            var tamanho = Console.ReadLine();
+            Console.WriteLine("Senha -> " + SenhaHelper.GeraSenhaAutomatico(Convert.ToInt32(tamanho)));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GeraSenhaAutomatico/SenhaHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace GeraSenhaAutomatico
+{
+    public static class SenhaHelper
+    {
+        public static string GeraSenhaAutomatico(int tamanho)
+        {
+            var chars = "abcdefghijklmnopqrstuvxyzçABCDEFGHIJKLMNOPQRSTUVWXYZÇ0123456789";
+            var random = new Random();
+            var senha = new string(Enumerable.Repeat(chars, tamanho).Select(s => s[random.Next(s.Length)]).ToArray());
+            return senha;
+        }
+    }
+}
